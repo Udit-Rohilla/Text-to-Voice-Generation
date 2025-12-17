8b8937a54888d19fb56734760b9d9ea65d292c21
Udit <udit.rohilla30@gmail.com>
2025-12-18 02:20:12 +0530

    Add root and health routes for production API visibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: proc { [200, {}, ["Text to Voice Generation API is running"]] }
+  root to: proc { [200, { "Content-Type" => "application/json" }, [{ status: "ok", service: "text-to-voice-generation" }.to_json]] }
 
-  get "/health", to: proc { [200, {}, ["OK"]] }
+  get "/health", to: proc { [200, { "Content-Type" => "application/json" }, ["OK"]] }
 
   namespace :api do
     namespace :v1 do
