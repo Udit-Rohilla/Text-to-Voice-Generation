9298a9575c49a85a550e49f8133a093a1c194286
Udit <udit.rohilla30@gmail.com>
2025-12-18 02:16:18 +0530

    Add root and health endpoints for production API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: proc { [200, {}, ["Text to Voice Generation API is running"]] }
+
+  get "/health", to: proc { [200, {}, ["OK"]] }
+
   namespace :api do
     namespace :v1 do
       resources :voice_requests, only: [:create, :show, :index]
